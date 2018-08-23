5e1d11d9ded474ec789c7655eb272b535eb71af0
Jackie <JGtheophila19@gmail.com>
2018-08-23 13:51:39 -0400

    Update App.js

--- react-playlist-master/myapp/src/App.js
+++ react-playlist-master/myapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas'
 
 class App extends Component {
   render() {
@@ -6,6 +7,8 @@ class App extends Component {
       <div className="App">
        <h1>My first React app!</h1>
        <p>Welcome :)</p>
+       <Ninjas name="Ryu" age="25" belt="black"/>
+       <Ninjas name="Yoshi" age="30" belt="green"/>
       </div>
     );
   }

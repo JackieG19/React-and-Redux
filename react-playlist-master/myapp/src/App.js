c1bde82031d3cb151082467e4931a25e51ecafa9
Jackie <JGtheophila19@gmail.com>
2018-08-27 21:57:02 -0400

    Add files via upload

--- react-playlist-master/myapp/src/App.js
+++ react-playlist-master/myapp/src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas'
+import AddNinija from './AddNinja';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: "Ryu", age: 30, belt: 'black', id: 1 },
+      { name: "Yoshi", age 20, belt: 'green', id: 2 },
+      { name: "Crystal", age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+
+  addNinja = (ninja) => {
+    // console.log(ninja);
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    // console.log(id)
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
   render() {
     return (
       <div className="App">
        <h1>My first React app!</h1>
        <p>Welcome :)</p>
+       <Ninjas  deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+       <AddNinija addNinja={HTMLDListElement.addNinja}>
       </div>
     );
   }

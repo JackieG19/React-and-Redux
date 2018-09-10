bc0b8ad9a4e59065727d27ffd2aec541f03b14bb
Jackie <jacgut19@gmail.com>
2018-09-10 16:06:38 +0000

    #25-TheReactRouter

---  React-Redux-Complete/poketimes/src/App.js
+++  React-Redux-Complete/poketimes/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Navbar from './components/Navbar'
+import { BrowserRouter,  } from 'react-router-dom'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Navbar/>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/contact" component={Contact}/>
+          <Narbar />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
         </div>
-      </BrowserRouter>
+      </BrowserRouter> 
     );
   }
 }

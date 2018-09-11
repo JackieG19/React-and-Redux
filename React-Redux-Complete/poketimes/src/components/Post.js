73569bf9863e4d7e0a98bae84e4118c03e87daee
Jackie <JGtheophila19@gmail.com>
2018-09-11 12:49:45 -0400

    Update Post.js

--- React-Redux-Complete/poketimes/src/components/Post.js
+++ React-Redux-Complete/poketimes/src/components/Post.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Post extends Component {
-    componentDidMount(){
-        //console.log(this.props)
         state = {
-            id: null
+            post: null
         }
+    componentDidMount(){
         let id = this.props.match.params.post_id;
-        this.setState({
-            id: id
+        axios.get('https://jsonplaceholder.typicode.com/posts' + id) 
+        .then(res => {
+           this.setState({
+               post: res.data
+           }) 
+           console.log(res)
         })
     }
     render() {
         return (
             <div className="container">
-                <h4>{this.state.id}</h4>
+                <h4></h4>
             </div>
         )
     }
 }
-
 export default Post

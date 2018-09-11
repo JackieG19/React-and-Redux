957293def93fa44a85bbff9a2dc64dfc10b6e1c2
Jackie <JGtheophila19@gmail.com>
2018-09-11 11:43:01 -0400

    Add files via upload

---
+++ React-Redux-Complete/poketimes/src/components/Post.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Post extends Component {
+    componentDidMount(){
+        //console.log(this.props)
+        state = {
+            id: null
+        }
+        let id = this.props.match.params.post_id;
+        this.setState({
+            id: id
+        })
+    }
+    render() {
+        return (
+            <div className="container">
+                <h4>route param</h4>
+            </div>
+        )
+    }
+}
+
+export default Post

6b4d93ba8f38af4ebf0a9759a64121edf9d094c7
Jackie <JGtheophila19@gmail.com>
2018-09-13 12:55:39 -0400

    redux action

--- redux-basic/js/index.js
+++ redux-basic/js/index.js
@@ -1,9 +1,35 @@
 const { createStore } = Redux;
+
 const initState = {
   todos: [],
   posts: []
 }
+
 function myreducer(state = initState, action){
-  
+  console.log(action, state);
 }
+
 const store = createStore(myreducer);
+
+const todoAction = { type: 'ADD_TODO', todo: 'buy milk' };
+
+store.dispatch(todoAction)
+
+
+// Console
+
+//  Object {
+//  type: "@@redux/INIT9.k.9.e.l.8"
+//  } Object {
+//    posts: []
+//    todos: []
+//  }
+// ___________________________________
+
+//  Object {
+//  todo: "buy milk",
+//  type: "ADD_TODO"
+//  } Object {
+//    posts: []
+//    todos: []
+//  }

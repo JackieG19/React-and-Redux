b771ea3fd4604870851049c3bcdc4c53d40cd6b5
Jackie <JGtheophila19@gmail.com>
2018-09-18 11:38:50 -0400

    console logging the action of the button

--- React-Redux-Complete/poketimes/src/reducers/rootReducer.js
+++ React-Redux-Complete/poketimes/src/reducers/rootReducer.js
@@ -1,10 +1,16 @@
 const initState = {
-    posts: []
+    posts: [
+        {id: '1', title: 'Squirtle laid an egg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+        {id: '2', title: 'Charmander laid an egg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+        {id: '3', title: 'a helix fossil was found', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
     return state;
 }
+
+console.log(action);
 
 export default rootReducer
 
